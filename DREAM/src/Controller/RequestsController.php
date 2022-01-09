593168392f6de5418ebeb011c76742b44fda490e
Chiara-Magri <chiara1.magri@mail.polimi.it>
2022-01-09 19:24:46 +0100

    Added RequestController

--- DREAM/src/Controller/RequestsController.php
+++ DREAM/src/Controller/RequestsController.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\HelpRequests\HelpRequest;
+
+#[Route('/my_requests', name: 'my_requests_')]
+class RequestsController extends AbstractController{
+
+    #[Route('/{helpRequest<\d+>?}', name: 'index', methods: ['GET'])]
+    // returns MyRequests page, req is the selected request for which details should be shown
+    public function index(Request $request, ?HelpRequest $helpRequest): Response {
+        return true;
+    }
+}
