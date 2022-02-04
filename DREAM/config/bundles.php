dc427ec92353f65b03c62603c43d6059c36b701e
Chiara-Magri <chiara1.magri@mail.polimi.it>
2022-02-04 18:48:34 +0100

    Set up test environment

--- DREAM/config/bundles.php
+++ DREAM/config/bundles.php
@@ -14,4 +14,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
